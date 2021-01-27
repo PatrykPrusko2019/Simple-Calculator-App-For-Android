f5a1687fc0273468402df39921f9efb561ed7cca
PatrykPrusko2019 <patrykprusko@gmail.com>
2021-01-27 17:03:44 +0100

    adds changing

--- SimpleCalculator/app/src/main/java/com/patrykprusko/simplecalculator/MainActivity.java
+++ SimpleCalculator/app/src/main/java/com/patrykprusko/simplecalculator/MainActivity.java
@@ -12,24 +12,24 @@ import androidx.annotation.NonNull;
 /**
  * A simple calculator app for android. Created GUI for vertical and horizontal activity. Consists of 20 buttons:
  * - 16 basic
- * - 4 additional
- * Application operation:
+ * - 4 additional :
+ * Application operation: Clear button -> C , Negation button -> NEG , Modulo button -> MOD , Square root button -> √x
  * - basic calculations addition, subtraction, division, multiplication.
  *
  * Additional buttons created:
- * 1. Clear button: clears the fields result, newNumber, operations.
- * 2. Negation button:
+ * 1. Clear button ( C ) : clears the fields result, newNumber, operations.
+ * 2. Negation button ( NEG ) :
  * * if empty newNumber field adds "-".
  * * if the minus sign alone in the newNumber field returns an empty field.
  * * if it has a value other than 0, it changes the sign of the given number.
  * * if 0 is present, it returns 0 (if -0.0 occurs, it will return 0).
  *
- * 3. Modulo button (MOD):
+ * 3. Modulo button ( MOD ) :
  * * when there is a value in the newNumber field, it runs the performOperation (value, "%") method:
  * calculates the final result, changes the current "%" sign, sets the result field, clears the newNumber field.
  * * when there is no new value, it sets the current sign of the operator "%"
  *
- * 4. Square root button:
+ * 4. Square root button ( √x ) :
  * * if there is a new value in the newNumber field, do the square root, the operator changes to "=".
  * * if there is no new value, but there is a result, then root.
  * * if the result of division by zero, then it is not a root.
